c7e99ac883f645c7760b3aa1568a9b9e689c9a7c
hy403-prog <perezhainer76@gmail.com>
2026-06-10 16:53:57 -0600

    Add files via upload

---
+++ PracticaParcial/PracticaParcial/Producto.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace PracticaParcial
+{
+    internal class Producto
+    {
+        public int IdProducto { get; set; }
+        public string Nombre { get; set; }
+        public decimal Precio { get; set; }
+        public int CantidadStock { get; set; }
+        public int IdProveedor { get; set; }
+        public int IdCategoria { get; set; }
+
+        public Producto() { }
+
+        public Producto(int idProducto, string nombre, decimal precio,
+                        int cantidadStock, int idProveedor, int idCategoria)
+        {
+            this.IdProducto = idProducto;
+            this.Nombre = nombre;
+            this.Precio = precio;
+            this.CantidadStock = cantidadStock;
+            this.IdProveedor = idProveedor;
+            this.IdCategoria = idCategoria;
+        }
+
+        // GUARDAR PRODUCTO
+        public int GuardarProducto()
+        {
+            int resultado = 0;
+
+            SqlConnection conexSQL = Conexion.GetConexion().crearConexion();
+
+            string queryInsert = @"
+                INSERT INTO Productos 
+                (Nombre, Precio, CantidadStock, IdProveedor, IdCategoria) 
+                VALUES
+                (@nombre, @precio, @stock, @idproveedor, @idcategoria)
+            ";
+
+            SqlCommand cmd = new SqlCommand(queryInsert, conexSQL);
+
+            cmd.Parameters.AddWithValue("@nombre", this.Nombre);
+            cmd.Parameters.AddWithValue("@precio", this.Precio);
+            cmd.Parameters.AddWithValue("@stock", this.CantidadStock);
+            cmd.Parameters.AddWithValue("@idproveedor", this.IdProveedor);
+            cmd.Parameters.AddWithValue("@idcategoria", this.IdCategoria);
+
+            conexSQL.Open();
+            resultado = cmd.ExecuteNonQuery();
+            conexSQL.Close();
+
+            return resultado;
+        }
+
+        // LISTAR PRODUCTOS
+        public DataTable ListarProductos()
+        {
+            SqlConnection conex = Conexion.GetConexion().crearConexion();
+
+            DataTable dtProductos = new DataTable();
+
+            string consulta = @"
+                SELECT 
+                    p.IdProducto,
+                    p.Nombre,
+                    p.Precio,
+                    p.CantidadStock,
+                    pr.Nombre AS NombreProveedor,
+                    c.Nombre AS NombreCategoria
+                FROM Productos p
+                INNER JOIN Proveedores pr
+                    ON p.IdProveedor = pr.IdProveedor
+                INNER JOIN Categoria c
+                    ON p.IdCategoria = c.IdCategoria
+            ";
+
+            SqlDataAdapter daProducto = new SqlDataAdapter(consulta, conex);
+
+            daProducto.Fill(dtProductos);
+
+            conex.Close();
+
+            return dtProductos;
+        }
+    }
+}
---
+++ PracticaParcial/PracticaParcial/Venta.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace PracticaParcial
+{
+    internal class Venta
+    {
+        public int IdVenta { get; set; }
+        public int IdCliente { get; set; }
+        public DateTime FechaVenta { get; set; }
+        public decimal DescuentoAplicado { get; set; }
+        public decimal MontoTotal { get; set; }
+
+        public Venta() { }
+
+        public Venta(int idCliente, decimal descuentoAplicado, decimal montoTotal)
+        {
+            this.IdCliente = idCliente;
+            this.FechaVenta = DateTime.Now;
+            this.DescuentoAplicado = descuentoAplicado;
+            this.MontoTotal = montoTotal;
+        }
+
+        // GUARDAR VENTA
+        public int GuardarVenta()
+        {
+            int resultado = 0;
+
+            SqlConnection conexSQL = Conexion.GetConexion().crearConexion();
+
+            string queryInsert = @"
+                INSERT INTO Ventas
+                (IdCliente, FechaVenta, DescuentoAplicado, MontoTotal)
+                VALUES
+                (@idcliente, @fechaventa, @descuento, @monto)
+            ";
+
+            SqlCommand cmd = new SqlCommand(queryInsert, conexSQL);
+
+            cmd.Parameters.AddWithValue("@idcliente", this.IdCliente);
+            cmd.Parameters.AddWithValue("@fechaventa", this.FechaVenta);
+            cmd.Parameters.AddWithValue("@descuento", this.DescuentoAplicado);
+            cmd.Parameters.AddWithValue("@monto", this.MontoTotal);
+
+            conexSQL.Open();
+
+            resultado = cmd.ExecuteNonQuery();
+
+            conexSQL.Close();
+
+            return resultado;
+        }
+
+        // OBTENER ÚLTIMA VENTA
+        public int ObtenerUltimaVenta()
+        {
+            int idVenta = 0;
+
+            SqlConnection conexSQL = Conexion.GetConexion().crearConexion();
+
+            string query = "SELECT MAX(IdVenta) FROM Ventas";
+
+            SqlCommand cmd = new SqlCommand(query, conexSQL);
+
+            conexSQL.Open();
+
+            object result = cmd.ExecuteScalar();
+
+            if (result != DBNull.Value)
+            {
+                idVenta = Convert.ToInt32(result);
+            }
+
+            conexSQL.Close();
+
+            return idVenta;
+        }
+
+        // LISTAR VENTAS
+        public DataTable ListarVentas()
+        {
+            SqlConnection conex = Conexion.GetConexion().crearConexion();
+
+            DataTable dtVentas = new DataTable();
+
+            string consulta = @"
+                SELECT 
+                    v.IdVenta,
+                    c.Nombre AS NombreCliente,
+                    v.FechaVenta,
+                    v.DescuentoAplicado,
+                    v.MontoTotal
+                FROM Ventas v
+                INNER JOIN Cliente c
+                    ON v.IdCliente = c.IdCliente
+                ORDER BY v.FechaVenta DESC
+            ";
+
+            SqlDataAdapter daVenta = new SqlDataAdapter(consulta, conex);
+
+            daVenta.Fill(dtVentas);
+
+            conex.Close();
+
+            return dtVentas;
+        }
+
+        // OBTENER DETALLES DE VENTA
+        public DataTable ObtenerDetallesVenta(int idVenta)
+        {
+            SqlConnection conex = Conexion.GetConexion().crearConexion();
+
+            DataTable dtDetalles = new DataTable();
+
+            string consulta = @"
+                SELECT 
+                    dv.IdDetalle,
+                    p.Nombre AS NombreProducto,
+                    dv.CantidadVendida,
+                    dv.PrecioUnitario,
+                    dv.SubTotal
+                FROM DetalleVentas dv
+                INNER JOIN Productos p
+                    ON dv.IdProducto = p.IdProducto
+                WHERE dv.IdVenta = @idventa
+            ";
+
+            SqlDataAdapter daDetalle = new SqlDataAdapter(consulta, conex);
+
+            daDetalle.SelectCommand.Parameters.AddWithValue("@idventa", idVenta);
+
+            daDetalle.Fill(dtDetalles);
+
+            conex.Close();
+
+            return dtDetalles;
+        }
+    }
+}
